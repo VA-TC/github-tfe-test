bab86a4a969f60fbfe2ad41bf97fbfd428bc3bee
Timothy Creech <127444822+VA-TC@users.noreply.github.com>
2025-09-12 12:29:31 -0400

    Update main.tf
    Testing with a slightly more complex resource

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
-resource "null_resource" "test" {}
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+provider "random" {}
+# Generate a random string of 8 characters
+resource "random_string" "example" {
+  length  = 8
+  upper   = true
+  lower   = true
+  number  = true
+  special = false
+}
+output "random_string_value" {
+  value = random_string.example.result
+}
